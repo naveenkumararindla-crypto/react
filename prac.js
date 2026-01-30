fcaed95a9fc5a278f68ae4cd177c58f086d14202
Naveen <naveenkumararindla@gmail.com>
2026-01-30 15:54:11 +0530

    my first commit

---
+++ prac.js
@@ -0,0 +1,5 @@
+const element=React.createElement("h1",{id:"mahesh",className:"baburao",style:{backgroundColor:"blue", color:"orange"}},"hello coder army");
+const element2=React.createElement("h2",{},"bokula malllesh");
+const div=React.createElement("div",{},[element,element2]);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(div);
